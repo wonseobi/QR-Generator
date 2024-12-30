bbb4ee4cdbb3b7248c12a5f1d6457d1b9ad96f1a
Won Lee <wonseob2207@gmail.com>
2024-12-30 15:34:18 -0600

    finished project

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import inquirer from "inquirer";
 import qr from "qr-image";
-var qr = require('qr-image');
+import fs from "fs";
 
 inquirer
   .prompt([{
@@ -9,7 +9,12 @@ inquirer
   .then((answers) => {
     const url = answers.URL;
     var qr_svg = qr.image('I love QR!');
-    qr_svg.pipe(require('fs').createWriteStream('qr_img.png'));
+    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+
+    fs.writeFile("URL.txt", url, (err) => {
+        if (err) throw err;
+        console.log("The file has been saved!");
+    })
   })
   .catch((error) => {
     if (error.isTtyError) {
